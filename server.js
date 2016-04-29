f2f4ce352459a4f113fbbdf515ebf7fbfc2a023b
Jason Hudson <jason.hudson@ldschurch.org>
2016-04-29 13:34:10 -0600

    update some files

--- server.js
+++ server.js
@@ -2,18 +2,18 @@
 
 // IMPORTS
 var appName = 'scriptures_now',
-    http = require('http'),
-    https = require('https'),
-    request = require('request'),
-    express = require('express'),
-    requireDirectory = require('require-directory'),
-    bodyParser = require('body-parser'),
-    json = bodyParser.json,
-
-    middleware = requireDirectory(module, './server/middleware'),
+http = require('http'),
+https = require('https'),
+request = require('request'),
+express = require('express'),
+requireDirectory = require('require-directory'),
+bodyParser = require('body-parser'),
+json = bodyParser.json,
+//scripParser = require('lds-scripture-nlp-query-parser'),
+//middleware = requireDirectory(module, './server/middleware'),
 
 // Create the express app
-    app = express();
+app = express();
 
 
 http.globalAgent.maxSockets = 9999;
@@ -24,15 +24,17 @@ app.set('port', process.env.VCAP_APP_PORT || 4000);
 //Body Parser for dealing with POSTs
 //app.use(json());
 
+app.get("/api/v1/get-scripture", function(req, res) {
+    /*var obj = scripParser.parse(req.msg);*/
+});
 
 app.use(function(req, res, next){
-
+    console.log('Hello World');
     var returnBody;
     var queryParams = { book:"bofm", query:"faith"};
 
     var queryString = '';
-    for(var key in queryParams)
-    {
+    for(var key in queryParams) {
         queryString += "&" + key + "=" + queryParams[key];
     }
 
@@ -45,16 +47,13 @@ app.use(function(req, res, next){
     });
 
 
-    //TODO Parse returnBody to hit footnote
+//TODO Parse returnBody to hit footnote
     var footnoteURL = "https://www.lds.org/scriptures/footnote/detail?lang=eng&isReference=true&noteUri=/scriptures/bofm/w-of-m/1.11";
     request(footnoteURL, function (error, respoonse, body) {
         res.send(body); //TODO clean this up, make it nice
-
     });
 
-    //I don't think that calling request like this is async.
+//I don't think that calling request like this is async.
 });
-
-
 
 module.exports = app;
